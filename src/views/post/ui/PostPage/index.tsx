ba59bdff86010b0e5e20a1d2cf7a4b5df4befa86
moonkanghyeon <s25024@gsm.hs.kr>
2026-01-02 22:28:00 +0900

    feat: Post 컴포넌트에 Header 추가 및 코드 정리

--- src/views/post/ui/PostPage/index.tsx
+++ src/views/post/ui/PostPage/index.tsx
@@ -1,6 +1,7 @@
 import SortDropDown from '@/shared/ui/SortDropDown';
 import PostPreview from '@/shared/ui/PostPreview';
 import Arrow from '@/shared/assets/svg/Arrow';
+import Header from '@/widgets/header/ui';
 
 const postData = [
   { id: 1, author: '작성자', title: '사진제목', date: '2025-01-01' },
@@ -13,28 +14,31 @@ const postData = [
 
 export default function Post() {
   return (
-    <div className="flex justify-center">
-      <div className="flex w-[1144px] flex-col gap-7">
-        <div className="mt-20 flex items-end justify-between self-stretch">
-          <h1 className="text-h1 text-gray-900">게시판</h1>
-          <SortDropDown />
-        </div>
-        <div className="grid grid-cols-3 gap-x-6 gap-y-12">
-          {postData.map((post) => (
-            <PostPreview
-              key={post.id}
-              author={post.author}
-              title={post.title}
-              date={post.date}
-            />
-          ))}
-        </div>
-        <div className="mt-8 flex justify-center gap-5">
-          <Arrow direction="left" color="black" />
-          <p className="text-body1">1</p>
-          <Arrow direction="right" color="black" />
+    <>
+      <Header />
+      <div className="flex justify-center">
+        <div className="flex w-[1144px] flex-col gap-7">
+          <div className="mt-20 flex items-end justify-between self-stretch">
+            <h1 className="text-h1 text-gray-900">게시판</h1>
+            <SortDropDown />
+          </div>
+          <div className="grid grid-cols-3 gap-x-6 gap-y-12">
+            {postData.map((post) => (
+              <PostPreview
+                key={post.id}
+                member={post.author}
+                title={post.title}
+                date={post.date}
+              />
+            ))}
+          </div>
+          <div className="mt-8 flex justify-center gap-5">
+            <Arrow direction="left" color="black" />
+            <p className="text-body1">1</p>
+            <Arrow direction="right" color="black" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
