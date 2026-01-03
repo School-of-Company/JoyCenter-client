bc9d56c6019bd5f85e5cdfa4b072c0666c186be2
moonkanghyeon <s25024@gsm.hs.kr>
2026-01-03 23:02:11 +0900

    style: PostCard 컴포넌트 스타일 수정

--- src/shared/ui/PostCard/index.tsx
+++ src/shared/ui/PostCard/index.tsx
@@ -17,10 +17,10 @@ export default function PostCard({
 }: PostCardProps) {
   return (
     <div
-      className="cursor-pointer rounded-xl bg-gray-100 p-3"
+      className="cursor-pointer border-b border-b-gray-200 px-3"
       onClick={onClick}
     >
-      <div className="flex items-center justify-center rounded-lg bg-[#D9D9D9] p-3">
+      <div className="flex h-22.5 w-40 items-center justify-center rounded-lg bg-[#D9D9D9]">
         {imageUrl ? (
           <img
             src={imageUrl}
@@ -31,9 +31,9 @@ export default function PostCard({
           <Picture width="64px" height="42px" />
         )}
       </div>
-      <div className="mt-2 flex items-center justify-between px-3 py-2">
-        <p className="text-body2 font-semibold">{title}</p>
-        <p className="text-body5 text-gray-900">{member}</p>
+      <div className="mt-2 mb-2 flex items-center justify-between">
+        <p className="text-xs font-semibold">{title}</p>
+        <p className="text-[8px] text-gray-900">{member}</p>
       </div>
     </div>
   );
