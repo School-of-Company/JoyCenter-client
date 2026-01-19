61825e0c5ece8921064e3537809b6691399ca3a3
bae080311 <s24024@gsm.hs.kr>
2026-01-19 14:17:44 +0900

    fix: img style

--- src/widgets/main/ui/MainSection/index.tsx
+++ src/widgets/main/ui/MainSection/index.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image';
 
 export default function MainSection() {
   return (
-    <section className="w-screen">
+    <section className="w-full overflow-x-hidden">
       <Image
         src="/bgImg.png"
         alt="배경"
         width={1920}
         height={1080}
-        className="h-auto w-full"
+        className="block h-auto w-full max-w-full"
+        sizes="100vw"
         priority
       />
     </section>
