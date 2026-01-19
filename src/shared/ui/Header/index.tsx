a7de4feac7936671aa2df2ffe53a3a57a49b3e0b
bae080311 <s24024@gsm.hs.kr>
2026-01-19 14:24:03 +0900

    fix: header styel

--- src/shared/ui/Header/index.tsx
+++ src/shared/ui/Header/index.tsx
@@ -1,23 +1,35 @@
+'use client'
+
 import Logo from '@/shared/assets/svg/Logo';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const pathname = usePathname();
+  const isMain = pathname === '/main';
+
   return (
-    <header className="border-main-yellow-200 flex w-full items-center justify-around border-b bg-white py-[5px]">
-      <Link href="" className="flex items-center">
+    <header
+      className={`flex w-full items-center justify-around py-[5px] ${
+        isMain
+          ? 'absolute top-0 left-0 z-50 bg-[rgba(255,255,255,0.50)]'
+          : 'relative bg-white border-b border-main-yellow-200'
+      }`}
+    >
+      <Link href="/main" className="flex items-center">
         <Logo />
       </Link>
       <nav className="flex items-center gap-14">
-        <Link href="" className="text-body1 text-main-yellow-950">
+        <Link href="" className='text-body1'>
           기관 소개
         </Link>
-        <Link href="/post" className="text-body1 text-main-yellow-950">
+        <Link href="/post" className='text-body1' >
           게시판
         </Link>
-        <Link href="/post-write" className="text-body1 text-main-yellow-950">
+        <Link href="/post-write" className='text-body1'>
           글쓰기
         </Link>
-        <Link href="" className="text-body1 text-main-yellow-950">
+        <Link href="" className='text-body1'>
           로그인
         </Link>
       </nav>
