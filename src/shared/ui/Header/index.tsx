8a13edd2eb36ab35ea2d8052606cecb89a419848
bae080311 <s24024@gsm.hs.kr>
2026-01-20 11:20:51 +0900

    fix: code style

--- src/shared/ui/Header/index.tsx
+++ src/shared/ui/Header/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import Logo from '@/shared/assets/svg/Logo';
 import Link from 'next/link';
@@ -13,23 +13,23 @@ export default function Header() {
       className={`flex w-full items-center justify-around py-[5px] ${
         isMain
           ? 'absolute top-0 left-0 z-50 bg-[rgba(255,255,255,0.50)]'
-          : 'relative bg-white border-b border-main-yellow-200'
+          : 'border-main-yellow-200 relative border-b bg-white'
       }`}
     >
       <Link href="/main" className="flex items-center">
         <Logo />
       </Link>
       <nav className="flex items-center gap-14">
-        <Link href="" className='text-body1'>
+        <Link href="" className="text-body1">
           기관 소개
         </Link>
-        <Link href="/post" className='text-body1' >
+        <Link href="/post" className="text-body1">
           게시판
         </Link>
-        <Link href="/post-write" className='text-body1'>
+        <Link href="/postWrite" className="text-body1">
           글쓰기
         </Link>
-        <Link href="" className='text-body1'>
+        <Link href="/signin" className="text-body1">
           로그인
         </Link>
       </nav>
--- src/widgets/main/ui/MainSection/index.tsx
+++ src/widgets/main/ui/MainSection/index.tsx
@@ -15,25 +15,16 @@ export default function MainSection() {
 
       <div className="relative z-10 flex min-h-svh w-full items-center">
         <div className="relative w-full px-6 text-white md:ml-24 md:max-w-xl md:px-0">
-          <div
-            className="pointer-events-none absolute
-              w-[720px] h-[360px]
-              rounded-[520px]
-              bg-[rgba(26,26,27,0.5)]
-              blur-[35px]
-              md:blur-[45px]
-              md:-top-[80px] md:-left-[150px]"
-          />
-          <div className="relative flex flex-col justify-center items-center text-center">
-            <p className="mb-3 text-h2 opacity-90">
-              더 기쁘게, 더 따뜻하게
-            </p>
-            <h1 className="mb-6 text-3xl font-bold leading-tight md:text-5xl">
+          <div className="pointer-events-none absolute h-[360px] w-[720px] rounded-[520px] bg-[rgba(26,26,27,0.5)] blur-[35px] md:-top-[80px] md:-left-[150px] md:blur-[45px]" />
+          <div className="relative flex flex-col items-center justify-center text-center">
+            <p className="text-h2 mb-3 opacity-90">더 기쁘게, 더 따뜻하게</p>
+            <h1 className="mb-6 text-3xl leading-tight font-bold md:text-5xl">
               더기쁨노인복지센터
             </h1>
-            <div className='h-px bg-white w-4/5 mb-6' />
+            <div className="mb-6 h-px w-4/5 bg-white" />
             <p className="text-sm leading-relaxed opacity-90 md:text-base">
-              어르신의 일상에 기쁨과 안심을 더해 정성으로 함께하겠습니다.<br />
+              어르신의 일상에 기쁨과 안심을 더해 정성으로 함께하겠습니다.
+              <br />
               TEL. 062-372-3525
             </p>
           </div>
