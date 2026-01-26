8de78e82bc8e6ccc8c0b9f9232765668ca53f947
bae080311 <s24024@gsm.hs.kr>
2026-01-26 19:01:09 +0900

    feat: google login

---
+++ src/widgets/signin/ui/SigninButtons/index.tsx
@@ -0,0 +1,23 @@
+import Google from '@/shared/assets/svg/Google';
+import Kakao from '@/shared/assets/svg/Kakao';
+import Link from 'next/link';
+
+export default function SigninButtons() {
+  return (
+    <>
+      <Link
+        href={`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=openid%20email%20profile&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&prompt=consent`}
+        className="text-body1 mt-4 mb-3 flex w-full items-center justify-around rounded-xl border border-gray-100 bg-white px-4 py-3"
+      >
+        <Google />
+        구글로 로그인
+        <div className="size-8" />
+      </Link>
+      <button className="text-body1 flex w-full items-center justify-around rounded-xl bg-[#FEE500] px-4 py-3">
+        <Kakao />
+        카카오로 로그인
+        <div className="size-8" />
+      </button>
+    </>
+  );
+}
