51be755b951a887194551917901e25ed8941664e
bae080311 <s24024@gsm.hs.kr>
2026-01-27 18:28:06 +0900

    feat: openGraph, canonical

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,23 @@ import { QueryProvider } from '@/shared/lib/QueryProvider';
 import { Toaster } from 'sonner';
 
 export const metadata: Metadata = {
-  title: '더기쁨노인복지센터',
-  description: '더 기쁘게, 더 따뜻하게 더기쁨 노인복지센터',
+  title: {
+    default: '더기쁨노인복지센터',
+    template: '%s | 더기쁨노인복지센터',
+  },
+  description: '더 기쁘게, 더 따뜻하게 더기쁨노인복지센터 공식 홈페이지',
+
+  alternates: {
+    canonical: 'https://joy-center-client.vercel.app',
+  },
+
+  openGraph: {
+    title: '더기쁨노인복지센터',
+    description: '어르신을 위한 복지·문화·프로그램 안내 플랫폼',
+    url: 'https://joy-center-client.vercel.app',
+    siteName: '더기쁨노인복지센터',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>
         <QueryProvider>
           <Header />
