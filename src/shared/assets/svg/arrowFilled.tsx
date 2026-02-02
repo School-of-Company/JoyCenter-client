fb1f487111b4c144ab934ebcac99e48fa809dd07
koreahghg <koreahghg@gmail.com>
2026-02-02 14:24:32 +0900

    feat: arrowFilled.tsx 추가

---
+++ src/shared/assets/svg/arrowFilled.tsx
@@ -0,0 +1,18 @@
+export default function ArrowFilled() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="32"
+      viewBox="0 0 16 32"
+      fill="none"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M13.5427 16.9475L6.00008 24.4902L4.11475 22.6049L10.7147 16.0049L4.11475 9.40486L6.00008 7.51953L13.5427 15.0622C13.7927 15.3122 13.9331 15.6513 13.9331 16.0049C13.9331 16.3584 13.7927 16.6975 13.5427 16.9475Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
