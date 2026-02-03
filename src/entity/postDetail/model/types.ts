ef15d3f1e21ccfbc3d4044008e6746b38e649ef3
moonkanghyeon <s25024@gsm.hs.kr>
2026-02-03 17:50:58 +0900

    feat: 게시글 상세 타입 정의 추가

---
+++ src/entity/postDetail/model/types.ts
@@ -0,0 +1,20 @@
+export interface PostDetail {
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  member: {
+    memberId: number;
+    email: string;
+  };
+  blocks: Array<{
+    blockId: number;
+    order: number;
+    type: 'TEXT';
+    text: string;
+    attachment: {
+      attachmentId: number;
+      attachmentsType: 'IMAGE';
+      attachmentUrl: string;
+    };
+  }>;
+}
