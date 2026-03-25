98f97d76bffc59e3de5137c66e09ddfa50f67983
bae080311 <s24024@gsm.hs.kr>
2026-03-25 18:13:02 +0900

    fix: url

--- src/app/(pages)/main/page.tsx
+++ src/app/(pages)/main/page.tsx
@@ -3,14 +3,14 @@ import MainView from '@/views/main/ui/MainView';
 
 export const metadata: Metadata = {
   alternates: {
-    canonical: 'https://joy-center-client.vercel.app/main',
+    canonical: 'https://www.joycenter.kr/main',
   },
   openGraph: {
     title: '더기쁨노인복지센터',
     description: '더 기쁘게, 더 따뜻하게 더기쁨노인복지센터 공식 홈페이지',
-    url: 'https://joy-center-client.vercel.app/main',
+    url: 'https://www.joycenter.kr/main',
     type: 'website',
-    images: [{ url: 'https://joy-center-client.vercel.app/bgImg.png', width: 1200, height: 630 }],
+    images: [{ url: 'https://www.joycenter.kr/bgImg.png', width: 1200, height: 630 }],
   },
 };
 
--- src/app/(pages)/post/page.tsx
+++ src/app/(pages)/post/page.tsx
@@ -5,12 +5,12 @@ export const metadata: Metadata = {
   title: '게시판',
   description: '더기쁨노인복지센터의 공지사항과 소식을 확인하세요.',
   alternates: {
-    canonical: 'https://joy-center-client.vercel.app/post',
+    canonical: 'https://www.joycenter.kr/post',
   },
   openGraph: {
     title: '게시판 | 더기쁨노인복지센터',
     description: '더기쁨노인복지센터의 공지사항과 소식을 확인하세요.',
-    url: 'https://joy-center-client.vercel.app/post',
+    url: 'https://www.joycenter.kr/post',
     type: 'website',
   },
 };
