4d85d84c072e148342aa08f6999f47608d8b48aa
bae080311 <s24024@gsm.hs.kr>
2026-04-03 15:48:08 +0900

    remove: signin

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -16,11 +16,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 0.8,
     },
-    {
-      url: `${BASE_URL}/signin`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.3,
-    },
   ];
 }
